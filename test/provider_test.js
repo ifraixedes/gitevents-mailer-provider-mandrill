130f79a3df628176052bd3a3d0ef92dd46dd39fc
Ivan Fraixedes <ifcdev@gmail.com>
2015-02-18 00:41:11 +0000

    Added first specs though tests descriptions

---
+++ test/provider_test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var test = require('tape');
+
+test('GitEvents-Mailer-Provider-Mandril', function (t) {
+  t.skip('exports a function');
+
+  test('initialise it', function (t) {
+    t.skip('exported function takes one options parameter which must be an object');
+    t.skip('exported function options parameter take message and sendOptions to replace defauls');
+    t.skip('exported function options parameter can optional contain template property');
+    t.skip('exported function options parameter template to be an string');
+
+    t.end();
+  });
+
+  t.end();
+});
