459dfcea5aa14ccc2a3452469776c43f6ffb5c8f
Ivan Fraixedes <ifcdev@gmail.com>
2015-03-03 00:09:43 +0000

    Fixed first test expects & added some more

--- test/provider_test.js
+++ test/provider_test.js
@@ -6,9 +6,12 @@ test('GitEvents-Mailer-Provider-Mandril', function (t) {
   t.skip('exports a function');
 
   test('initialise it', function (t) {
-    t.skip('exported function takes one options parameter which must be an object');
-    t.skip('exported function options parameter take message and sendOptions to replace defauls');
-    t.skip('exported function options parameter can optional contain template property');
+    t.skip('exported function takes API KEY as first parameter and an options object as second one');
+    t.skip('exported function API KEY parameters must be a string');
+    t.skip('exported function options parameter may have a message and sendOptions to replace defaults');
+    t.skip('exported function options parameter message must be an object');
+    t.skip('exported function options parameter sendOptions must be an object');
+    t.skip('exported function options parameter may have template property to allow to use mailchimp templates');
     t.skip('exported function options parameter template to be an string');
 
     t.end();
