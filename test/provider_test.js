ddd63888b9f465c540f2d37923caebed982db9cc
Ivan Fraixedes <ifcdev@gmail.com>
2015-03-18 00:29:06 +0000

    Implemented all test cases to do test-driver development

--- test/provider_test.js
+++ test/provider_test.js
@@ -1,21 +1,52 @@
 'use strict';
 
 var test = require('tape');
+var mandrillPlugin = require('../lib');
 
 test('GitEvents-Mailer-Provider-Mandril', function (t) {
-  t.skip('exports a function');
+  t.plan(1);
+  t.equal(typeof mandrillPlugin, 'function', 'exports a function');
 
+  /* eslint-disable no-shadow */
   test('initialise it', function (t) {
-    t.skip('exported function takes API KEY as first parameter and an options object as second one');
-    t.skip('exported function API KEY parameters must be a string');
-    t.skip('exported function options parameter may have a message and sendOptions to replace defaults');
-    t.skip('exported function options parameter message must be an object');
-    t.skip('exported function options parameter sendOptions must be an object');
-    t.skip('exported function options parameter may have template property to allow to use mailchimp templates');
-    t.skip('exported function options parameter template to be an string');
+    t.plan(10);
+    t.throws(mandrillPlugin.bind(null), /API KEY is required/i,
+             'exported function takes API KEY as first parameter and an optional options object as second one');
+    t.throws(mandrillPlugin.bind(null, 10), /API KEY must be a non-empty string/i,
+             'exported function API KEY parameters must be a string');
+    t.throws(mandrillPlugin.bind(null, ''), /API KEY must be a non-empty string/i,
+             'exported function API KEY parameters must be a non-empty string');
+    t.doesNotThrow(mandrillPlugin.bind(null, 'APIKEY'),
+                   'exported function return the object plugin when only one parameters, API KEY is provided');
+    t.doesNotThrow(mandrillPlugin.bind(null, 'APIKEY', {}),
+                   'exported function options parameter may have a message and sendOptions to replace defaults');
+    t.throws(mandrillPlugin.bind(null, 'APIKEY', {
+      message: 'this is wrong type'
+    }), /options message must be an object/i,
+    'exported function options parameter message must be an object');
 
-    t.end();
+    t.throws(mandrillPlugin.bind(null, 'APIKEY', {
+      sendOptions: 'this is wrong type'
+    }), /options sendOptions must be an object/i,
+    'exported function options parameter sendOptions must be an object');
+
+    var mPlugin = mandrillPlugin('APIKEY');
+    t.equal(Object.keys(mPlugin).length, 1, 'returns an object which only has one property');
+    t.equal(typeof mPlugin.provider, 'object', 'returns an object which must have `provider`');
+    t.equal(typeof mPlugin.provider.send, 'function', '`provider` object implements gitevents-mailer provider API');
+
+    test('to use mandrill templates', function (t) {
+      t.plan(6);
+      t.throws(mandrillPlugin.bind(null, 'APIKEY', { template: {} }), /options template must be a string/i,
+               'exported function options parameter template to be an string');
+      t.throws(mandrillPlugin.bind(null, 'APIKEY', { template: '' }), /options template must be a non-empty string/i,
+               'exported function options parameter template to be a non-empty string');
+
+      var mPlugin = mandrillPlugin('APIKEY', { template: 'name' });
+      t.equal(Object.keys(mPlugin).length, 2, 'returns an object which has two properties');
+      t.equal(typeof mPlugin.provider, 'object', 'returns an object which must have `provider`');
+      t.equal(typeof mPlugin.provider.send, 'function', '`provider` object implements gitevents-mailer provider API');
+      t.equal(typeof mPlugin.renderer, 'function', 'returns an object which must have `renderer`');
+    });
   });
-
-  t.end();
 });
